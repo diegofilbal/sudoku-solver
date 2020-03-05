f6d58a7b626d1bef7af9b055e762c6f23f79ee5d
Diego Filgueiras <diegofilbal@ufrn.edu.br>
2020-03-05 20:47:43 -0300

    added read_input()

--- sudoku_checker.cpp
+++ sudoku_checker.cpp
@@ -5,6 +5,19 @@
 #define EMPTY short(0)
 #define SIZE 9
 
+typedef short board_t[SIZE][SIZE];
+
+void read_input( board_t b )
+{
+    for(short j{0}; j < SIZE; j++)
+    {
+        for (short k{0}; k < SIZE; k++)
+        {
+            std::cin >> board[j][k];
+        }
+    }
+}
+
 void print( short b[SIZE][SIZE] ) // Prints the whole given matrix to the user
 {
     std::cout << "\n";
@@ -12,7 +25,6 @@ void print( short b[SIZE][SIZE] ) // Prints the whole given matrix to the user
     for(int i{0}; i < SIZE; i++)
     {
         if(i == 3 || i == 6) std::cout << "_____________________\n\n";
-        // if(i == 3 || i == 6) std::cout << "\n------|-------|-------\n\n";
 
         for(int j{0}; j < SIZE; j++)
         {
@@ -96,182 +108,14 @@ bool is_valid( short b[SIZE][SIZE] )
 
 int main(void)
 {
+    board_t board;
 
-/*
-    short boards[8][SIZE][SIZE]=
-    {
-
-        {
-            { 1, 3, 4, 6, 7, 8, 5, 9, 2 },
-            { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
-            { 5, 9, 8, 3, 4, 2, 1, 6, 7 },
-            { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
-            { 4, 2, 6, 8, 5, 3, 9, 7, 1 },
-            { 7, 1, 3, 4, 2, 9, 8, 5, 6 },
-            { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
-            { 2, 8, 7, 9, 1, 4, 6, 3, 5 },
-            { 3, 4, 5, 2, 8, 6, 7, 1, 9 },
-        },
-
-        {
-            { 1, 6, 2, 8, 5, 7, 4, 9, 3 },
-            { 5, 3, 4, 1, 2, 9, 6, 7, 8 },
-            { 7, 8, 9, 6, 4, 3, 5, 2, 1 },
-            { 4, 7, 5, 3, 1, 2, 9, 8, 6 },
-            { 9, 1, 3, 5, 8, 6, 7, 4, 2 },
-            { 6, 2, 8, 7, 9, 4, 1, 3, 5 },
-            { 3, 5, 6, 4, 7, 8, 2, 1, 9 },
-            { 2, 4, 1, 9, 3, 5, 8, 6, 7 },
-            { 8, 9, 7, 2, 6, 1, 3, 5, 4 },
-        },
-
-        {
-            { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
-            { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
-            { 1, 9, 8, 3, 4, 2, 5, 6, 7 },
-            { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
-            { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
-            { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
-            { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
-            { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
-            { 3, 4, 5, 2, 8, 6, 1, 7, 9 },
-        },
-
-        {
-            { 6, 2, 1, 9, 5, 7, 3, 4, 8 },
-            { 2, 7, 4, 1, 9, 8, 6, 3, 5 },
-            { 1, 8, 3, 4, 2, 9, 5, 6, 7 },
-            { 8, 9, 7, 6, 1, 5, 4, 2, 3 },
-            { 4, 6, 8, 5, 3, 2, 7, 9, 1 },
-            { 5, 4, 6, 7, 8, 3, 9, 1, 2 },
-            { 7, 3, 9, 2, 4, 1, 8, 5, 6 },
-            { 9, 1, 5, 3, 7, 6, 2, 8, 4 },
-            { 3, 5, 2, 8, 6, 4, 1, 7, 9 },
-        },
-
-        {
-            { 6, 2, 1, 9, 5, 7, 3, 4, 8 },
-            { 2, 7, 4, 1, 9, 8, 6, 3, 5 },
-            { 1, 8, 3, 4, 2, 9, 5, 6, 7 },
-            { 8, 9, 7, 6, 1, 5, 4, 2, 3 },
-            { 4, 6, 8, 5, 3, 2, 5, 9, 1 },
-            { 5, 4, 6, 7, 8, 3, 9, 1, 2 },
-            { 7, 3, 9, 2, 4, 1, 8, 5, 6 },
-            { 9, 3, 5, 3, 7, 6, 2, 8, 4 },
-            { 3, 5, 2, 8, 6, 4, 1, 7, 9 },
-        },
-
-        {
-            { 6, 2, 1, 9, 5, 7, 3, 4, 8 },
-            { 2, 7, 4, 1, 9, 8, 6, 3, 5 },
-            { 1, 8, 3, 4, 2, 19, 5, 6, 7 },
-            { 8, 9, 7, 6, 1, 5, 4, 2, 3 },
-            { 4, 6, 8, 5, 3, 2, 7, 9, 1 },
-            { 5, 4, 6, 7, 8, 3, 9, 1, 2 },
-            { 7, -3, 9, 2, 4, 1, 8, 5, 6 },
-            { 9, 1, 5, 3, 7, 6, 2, 8, 4 },
-            { 3, 5, 2, 8, 6, 4, 1, 7, 9 },
-        },
-
-        {
-            { 6, 2, 1, 9, 5, 7, 3, 4, 8 },
-            { 2, 7, 4, 1, 9, 8, 6, 3, 5 },
-            { 1, 8, 3, 4, 2, 9, 5, 6, 7 },
-            { 8, 9, 7, 6, 1, 5, 4, 2, 3 },
-            { 4, 6, 8, 5, 3, 2, 7, 9, 1 },
-            { 5, 4, 6, 7, 8, 3, 9, 1, 2 },
-            { 7, 3, 9, 2, 4, 1, 0, 5, 6 },
-            { 9, 1, 5, 3, 7, 6, 2, 8, 4 },
-            { 3, 5, 2, 8, 6, 4, 1, 7, 9 },
-        },
-
-        {
-            { 6, 2, 1, 9, 5, 7, 3, 4, 8 },
-            { 2, 7, 4, 1, 9, 8, 6, 3, 5 },
-            { 1, 8, 3, 4, 2, 9, 5, 6, 7 },
-            { 8, 9, 7, 3, 1, 5, 4, 2, 3 },
-            { 4, 6, 8, 5, 3, 2, 7, 9, 1 },
-            { 5, 4, 6, 7, 8, 6, 9, 1, 2 },
-            { 7, 3, 9, 2, 4, 1, 8, 5, 6 },
-            { 9, 1, 5, 3, 7, 6, 2, 8, 4 },
-            { 3, 5, 2, 8, 6, 4, 1, 7, 9 },
-        }
-    
-    };
-*/
-
-/*
-    std::cout << "Board: \n";
-    print( board1 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board1 ) << std::endl;
-    std::cout << "\n";
-
-    std::cout << "Board: \n";
-    print( board2 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board2 ) << std::endl;
-    std::cout << "\n";
-
-
-    std::cout << "Board: \n";
-    print( board3 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board3 ) << std::endl;
-    std::cout << "\n";
-
-
-    std::cout << "Board: \n";
-    print( board4 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board4 ) << std::endl;
-    std::cout << "\n";
-
-
-    std::cout << "Board: \n";
-    print( board5 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board5 ) << std::endl;
-    std::cout << "\n";
-
-
-    std::cout << "Board: \n";
-    print( board6 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board6 ) << std::endl;
-    std::cout << "\n";
-
-
-    std::cout << "Board: \n";
-    print( board7 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board7 ) << std::endl;
-    std::cout << "\n";
-
-
-    std::cout << "Board: \n";
-    print( board8 );
-    std::cout << "Is valid? " << std::boolalpha << is_valid( board8 ) << std::endl;
-    std::cout << "\n";
-*/
-
-    // TODO: Make the code below work, instead of the ugly code above.
-/*
-    int n_boards{8}; // Number of boards...
-    for ( int i{0}; i < n_boards ; ++i )
-    {
-        std::cout << "\nBoard #" << (i+1) << ": \n";
-        print( boards[i] );
-        std::cout << "\n>> Is valid? " << std::boolalpha << is_valid( boards[i] ) << std::endl;
-        std::cout << "\n";
-    }
-*/
-
-    short board[SIZE][SIZE];
     short i{0}; // Iterator for board counting
+    short data;
 
-    while(std::cin) // It keeps running while there is still data in the file
+    while(std::cin) // It keeps running while there is still data in the input
     {
-        for(short j{0}; j < SIZE; j++)
-        {
-            for (short k{0}; k < SIZE; k++)
-            {
-                std::cin >> board[j][k];
-            }
-        }
+        read_input(board);
 
         std::cout << "\nBoard #" << (i+1) << ": \n";
         print( board );
